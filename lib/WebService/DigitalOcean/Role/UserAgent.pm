ab09e58375e6be4e1525b10cccd5b0b9d9cbf16c
André Walker <andre@andrewalker.net>
2015-01-16 15:14:02 -0200

    Refactor UserAgent role
    Break the main method ``make_request`` into smaller subroutines, making
    it easier to write tests where stuff is mocked.

--- lib/WebService/DigitalOcean/Role/UserAgent.pm
+++ lib/WebService/DigitalOcean/Role/UserAgent.pm
@@ -16,68 +16,111 @@ has ua => (
 sub _build_ua {
     my ($self) = @_;
 
-    my $version = __PACKAGE__->VERSION;
-    $version ||= 'devel';
+    my $version = __PACKAGE__->VERSION || 'devel';
 
-    my $ua = LWP::UserAgent->new(
-        agent => 'WebService::DigitalOcean/' . $version,
+    my @headers = (
+        'Authorization' => 'Bearer ' . $self->token,
+        'Content-Type'  => 'application/json; charset=utf-8',
     );
 
-    $ua->default_header('Authorization' => 'Bearer ' . $self->token);
-    $ua->default_header('Content-Type' => 'application/json; charset=utf-8');
+    my $ua = LWP::UserAgent->new(
+        agent           => 'WebService::DigitalOcean/' . $version,
+        default_headers => HTTP::Headers->new(@headers),
+    );
 
     return $ua;
+}
+
+sub _build_request {
+    my ( $self, $method, $uri, $data ) = @_;
+
+    my $full_url     = $self->api_base_url . $uri;
+    my $encoded_data = $data ? JSON::encode_json($data) : undef;
+    my $headers      = undef;
+
+    return HTTP::Request->new( $method, $full_url, $headers, $encoded_data );
+}
+
+sub _send_request {
+    my ($self, $request) = @_;
+
+    my $response = $self->ua->request($request);
+
+    return $response;
 }
 
 sub make_request {
     my ($self, $method, $uri, $data) = @_;
 
-    my $response = $self->ua->request(
-        HTTP::Request->new(
-            $method,
-            $self->api_base_url . $uri,
-            undef,
-            $data ? JSON::encode_json($data) : undef
-        )
-    );
+    my $request  = $self->_build_request( $method, $uri, $data );
+    my $response = $self->_send_request($request);
 
     my $result = {
+        request_object  => $request,
         response_object => $response,
         is_success      => $response->is_success,
         status_line     => $response->status_line,
     };
 
-    if ($response->content_type eq 'application/json') {
-        my $decoded_response = JSON::decode_json($response->decoded_content);
+    my $ratelimit = $self->_get_ratelimit( $response->headers );
 
-        if (is_HashRef($decoded_response)) {
-            $result->{meta}  = delete $decoded_response->{meta};
-            $result->{links} = delete $decoded_response->{links};
+    my $content = $self->_get_content(
+        $response->content_type,
+        $response->decoded_content,
+    );
 
-            if (keys(%$decoded_response) == 1) {
-                my @key = keys %$decoded_response;
-                $result->{content} = delete $decoded_response->{$key[0]};
-            }
-            else {
-                $result->{content} = $decoded_response;
-            }
-        }
-        else {
-            $result->{content} = $decoded_response;
-        }
+    return { %$result, %$ratelimit, %$content };
+}
+
+sub _get_ratelimit {
+    my ($self, $headers) = @_;
+
+    my $limit = $headers->header('RateLimit-Limit');
+
+    if (!$limit) {
+        return {};
     }
 
-    if (my $ratelimit = $response->header('RateLimit-Limit')) {
-        $result->{ratelimit} = {
-            limit     => $ratelimit,
-            remaining => $response->header('RateLimit-Remaining'),
+    return {
+        ratelimit => {
+            limit     => $limit,
+            remaining => $headers->header('RateLimit-Remaining'),
             reset     => DateTime->from_epoch(
-                epoch => $response->header('RateLimit-Reset')
+                epoch => $headers->header('RateLimit-Reset')
             ),
-        };
+        }
+    };
+}
+
+sub _get_content {
+    my ($self, $content_type, $content) = @_;
+
+    if ($content_type ne 'application/json') {
+        warn "Unexpected Content-Type " . $content_type;
+        return {};
     }
 
-    return $result;
+    my $decoded_response = JSON::decode_json( $content );
+
+    if ( !is_HashRef($decoded_response) ) {
+        return { content => $decoded_response };
+    }
+
+    my $meta  = delete $decoded_response->{meta};
+    my $links = delete $decoded_response->{links};
+
+    my @values = values %$decoded_response;
+
+    my $c = scalar @values == 1
+          ? $values[0]
+          : $decoded_response
+          ;
+
+    return {
+        meta    => $meta,
+        links   => $links,
+        content => $c,
+    };
 }
 
 1;
@@ -86,7 +129,12 @@ sub make_request {
 
 Role used to make requests to the DigitalOcean API, and to format their response.
 
-=method $res = $do->make_request($method, $path[, $data)
+=method make_request
+
+    my $res = $self->make_request(POST => '/domains', {
+        name       => 'example.com',
+        ip_address => '12.34.56.78',
+    });
 
 =head3 Arguments
 
@@ -145,15 +193,5 @@ RateLimit headers parsed.
 Makes requests to the DigitalOcean, and parses the response.
 
 All requests made from other methods use L</make_request> to make them.
-
-    my $res = $self->make_request(POST => '/domains', {
-        name       => 'example.com',
-        ip_address => '12.34.56.78',
-    });
-
-B<Note:> this is how L<WebService::DigitalOcean::Role::Domains/domain_create>
-is implemented. You shouldn't use this method directly in your application
-whenever possible. It's kept as a public method only because the API isn't
-entirely implemented in the module yet.
 
 More info: L<< https://developers.digitalocean.com/#introduction >>.
